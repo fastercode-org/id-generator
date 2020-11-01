f0a473027c2d618b593c6aefdf962e29e73062de
huyaolong <mail@yaolong.net>
2020-11-01 14:28:38 +0800

    remove multiple instance support, and merge boot-1.x/2.x starter into the id-generator-spring-boot-starter.

--- id-generator-spring-boot2-starter/src/main/java/org/fastercode/idgenerator/spring/starter/IDGeneratorProperties.java
+++
@@ -1,8 +0,0 @@
-package org.fastercode.idgenerator.spring.starter;
-
-import lombok.Data;
-import org.fastercode.idgenerator.core.IDGenDistributedConfig;
-
-@Data
-public class IDGeneratorProperties extends IDGenDistributedConfig {
-}
